f0a0b159f694bda8e7949ee4020d5b7b2c04a600
Papa <mbr0969@gmail.com>
2016-02-22 15:19:13 +0300

    цвета радуги в обратном порядке

---
+++ Rainbow/src/RainbowColor.java
@@ -0,0 +1,11 @@
+public class RainbowColor {
+
+    public static void main(String[] args) {
+
+        String[] rainbow = {"Red","Orange","Yellow","Green","Cyan", "Blue","Purple"};
+
+      for(  int i = rainbow.length - 1 ; i >=0 ; i-- ) {
+            System.out.println(rainbow[i]);
+      }
+    }
+}
