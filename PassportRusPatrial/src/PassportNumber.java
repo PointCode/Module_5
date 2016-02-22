9e1cdcd9ba5ea0802e61fd2214c33af791d4b451
Papa <mbr0969@gmail.com>
2016-02-22 15:19:13 +0300

    Серии паспортов граждан РФ

---
+++ PassportRusPatrial/src/PassportNumber.java
@@ -0,0 +1,11 @@
+public class PassportNumber {
+    public static void main(String[] args) {
+
+        String[] passportNumber = {"699000","699001","699002","699003","699004","699005","699006","699007"};
+
+        for (String number : passportNumber ) {
+            System.out.println(number);
+        }
+
+    }
+}
