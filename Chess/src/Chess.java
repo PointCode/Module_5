7dcbd9389692f36ad12611a9a44d011fc294f6dc
papa <mbr0969@gmail.com>
2016-03-02 23:36:10 +0300

    Шахмотная доска исправления

--- Chess/src/Chess.java
+++ Chess/src/Chess.java
@@ -1,31 +1,30 @@
 public class Chess {
     public static void main(String[] args) {
 
-        String[][] chess = new String[2][8];
+        String[][] chess = new String[8][8];
 
-        chess[0][0] = "A";
-        chess[0][1] = "B";
-        chess[0][2] = "C";
-        chess[0][3] = "D";
-        chess[0][4] = "E";
-        chess[0][5] = "F";
-        chess[0][6] = "G";
-        chess[0][7] = "H";
-        chess[1][0] = "1";
-        chess[1][1] = "2";
-        chess[1][2] = "3";
-        chess[1][3] = "4";
-        chess[1][4] = "5";
-        chess[1][5] = "6";
-        chess[1][6] = "7";
-        chess[1][7] = "8";
+        Character letter = 'A';
+        Character number = '1';
 
+        int codeLettre = (int) letter;
+        int codeNumber = (int) number;
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(chess[0][i] + chess[1][j] + " ");
+        for (int i = 0; i < chess.length; i++) {
+
+            number = '1';
+            codeNumber = (int) number;
+
+            for (int j = 0; j < chess.length; j++) {
+
+                chess[i][j] = Character.toString(letter) + Character.toString(number);
+                System.out.print(chess[i][j] + " ");
+                codeNumber++;
+                number = (char) codeNumber;
             }
+
             System.out.println();
+            codeLettre++;
+            letter = (char) codeLettre;
         }
     }
 
