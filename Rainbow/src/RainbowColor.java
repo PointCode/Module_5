52dea92a843c6ed8f6925dac7f3200a9382eca17
papa <mbr0969@gmail.com>
2016-03-07 00:50:24 +0300

    цвета радуги исправления добавлен стэк

--- Rainbow/src/RainbowColor.java
+++ Rainbow/src/RainbowColor.java
@@ -3,16 +3,16 @@ public class RainbowColor {
     public static void main(String[] args) {
 //--- иницилиируем массив цветами радуги
         String[] rainbow = {"Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Purple"};
-        int length = rainbow.length;
 
-//---создаем класс реализующий стэк массива цвито радуги и заполняем его
-        StackRainbow stackRainbow = new StackRainbow(length);
-            for (int i = 0; i < length; ++i) {
+
+//---создаем класс реализующий стэк массива цвитов радуги и заполняем его
+        StackRainbow stackRainbow = new StackRainbow(rainbow.length);
+            for (int i = 0; i <rainbow.length; ++i) {
                 stackRainbow.addElement(rainbow[i]);
             }
 //---радуга на небе
         System.out.printf("");
-        for (int i = 0; i < stackRainbow.mSize;++i ) {
+        for (int i = 0; i < rainbow.length;++i ) {
             System.out.println(stackRainbow.readElement(i));
         }
 //--- раудга растворяется
